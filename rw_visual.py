9e5877c4b1b149f320a78c4f6edc98ae77ca6504
brad-wilkes <rbwilkes@yahoo.com>
2024-11-17 11:47:48 -0600

    randomwalk practice

---
+++ rw_visual.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+from random_walk import Randomwalk
+
+# Make a random walk
+rw = Randomwalk()
+rw.fill_walk()
+
+# Plot the points in the walk
+plt.style.use('classic')
+fig, ax = plt.subplots()
+ax.scatter(rw.x_values, rw.y_values, s=15)
+plt.show()
